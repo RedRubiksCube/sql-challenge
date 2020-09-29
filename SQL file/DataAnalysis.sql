2253caa9140f5c636f93ebc0d2cb7b3ffe24e74f
RedRubiksCube <45070667+RedRubiksCube@users.noreply.github.com>
2020-09-29 14:38:14 -0400

    Working save

--- SQL file/DataAnalysis.sql
+++ SQL file/DataAnalysis.sql
@@ -17,22 +17,29 @@ WHERE hire_date > '1985-12-31' AND hire_date < '1987-01-01';
 --department number, department name, the manager's employee number, last name, first name.
 
 SELECT dept.dept_no, dept.dept_name, employee.emp_no, employee.first_name, employee.last_name
-FROM employees employee
-JOIN dept_manager dept_m ON dept_m.emp_no = employee.emp_no
-JOIN departments dept ON dept.dept_no = dept_m.depo_no
+FROM employees AS employee
+JOIN dept_manager AS dept_m ON dept_m.emp_no = employee.emp_no
+JOIN departments AS dept ON dept.dept_no = dept_m.depo_no
 order by employee.last_name
 
 
--- Question 4
--- List the department of each employee with the following information: 
--- employee number, last name, first name, and department name.
 
+
+-- Question 5
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT employee.first_name, employee.last_name, employee.sex
+FROM employees AS employee
+WHERE employee.first_name = 'Hercules'
+AND substring(employee.last_name from 1 for 1) = 'B'
 
 -- Question 6
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT employee.emp_no, employee.last_name, employee.first_name, department.dept_name
-FROM employees employee
+FROM employees AS employee
 JOIN dept_emp dep ON dep.emp_no = employee.emp_no
 JOIN departments department ON department.dept_no = dep.dept_no
 WHERE department.dept_name = 'Sales'
+
+
+
 
